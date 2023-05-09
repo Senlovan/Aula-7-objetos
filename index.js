ea72b77dc4728a4a9dea82db41b941fb31b1a230
Senlovan <maiconmattossenlovan@gmail.com>
2023-05-09 19:03:42 -0300

    Subindo Aula 7

---
+++ index.js
@@ -0,0 +1,44 @@
+/*const nome = "Maria"
+const idade = 20
+const endereço = "rua do bobo"
+
+const usuario = {
+    nome:"Maria",
+    idade:20,
+    endereço:["rua do bobo , numero 0",
+    "Rua qualquer , numero 120"
+]
+}
+console.log(usuario);
+console.log(usuario.nome);*/
+
+// dentro de objetos se usa : ao invés de =
+
+
+
+/*const filme = {
+    nomeDoFilme: "A Hora do Pesadelo",
+    direção: "Wes Craven",
+    anoDeLançamento: "1986",
+    elenco:["Robert Englund", "Heather Langenkamp", "Johnny Depp"],
+assistido: true
+}
+console.log(filme.nomeDoFilme);
+console.log(filme.direção);
+console.log(filme.anoDeLançamento);
+console.log(filme.elenco); */
+
+
+const pessoa = {
+    nome:"Maicon",
+    idade:"27",
+    generoMusical:"Hip Hop",
+    endereço: {
+        rua: "rua do bobo",
+        numero: 0,
+        cidade: "São Leopoldo",
+        estado: "RS"
+    }
+}
+/*console.log("O Nome da Pessoa é ,", `${pessoa.nome}`,"Ela Tem:", `${pessoa.idade}`,"e gosta muito de:",`${pessoa.generoMusical}`)*/
+console.log(pessoa.endereço);
